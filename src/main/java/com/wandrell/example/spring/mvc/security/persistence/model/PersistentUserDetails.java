07b9a4b3b264f7b062ba97945c3152d534fba123
Bernardo <programming@wandrell.com>
2017-09-01 20:32:21 +0200

    Added flags to DB

--- src/main/java/com/wandrell/example/spring/mvc/security/persistence/model/PersistentUserDetails.java
+++ src/main/java/com/wandrell/example/spring/mvc/security/persistence/model/PersistentUserDetails.java
@@ -22,23 +22,28 @@ public class PersistentUserDetails implements UserDetails {
     @Transient
     private static final long serialVersionUID = 4807136960800402795L;
 
+    @Column(name = "enabled", nullable = false)
+    private Boolean           enabled;
+
+    @Column(name = "expired", nullable = false)
+    private Boolean           expired;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
     private final Long        id               = null;
 
-    @Column(name = "name", nullable = false, unique = true)
-    private String            username;
+    @Column(name = "locked", nullable = false)
+    private Boolean           locked;
 
     @Column(name = "password", nullable = false)
     private String            password;
 
+    @Column(name = "name", nullable = false, unique = true)
+    private String            username;
+
     public PersistentUserDetails() {
         super();
-    }
-
-    public Long getId() {
-        return id;
     }
 
     @Override
@@ -46,12 +51,20 @@ public class PersistentUserDetails implements UserDetails {
         return Collections.emptyList();
     }
 
-    public void setUsername(final String username) {
-        this.username = username;
+    public Boolean getEnabled() {
+        return enabled;
     }
 
-    public void setPassword(final String password) {
-        this.password = password;
+    public Boolean getExpired() {
+        return expired;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Boolean getLocked() {
+        return locked;
     }
 
     @Override
@@ -66,22 +79,42 @@ public class PersistentUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return !expired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return !expired;
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return enabled;
+    }
+
+    public void setEnabled(final Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setExpired(final Boolean expired) {
+        this.expired = expired;
+    }
+
+    public void setLocked(final Boolean locked) {
+        this.locked = locked;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
     }
 
 }
