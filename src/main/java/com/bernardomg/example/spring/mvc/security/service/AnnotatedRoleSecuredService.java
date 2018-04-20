19c994cb7bad9fd54e8d8bb13decb604a8c80771
Bernardo <programming@wandrell.com>
2018-04-20 20:34:36 +0200

    Added javadocs

--- src/main/java/com/bernardomg/example/spring/mvc/security/service/AnnotatedRoleSecuredService.java
+++ src/main/java/com/bernardomg/example/spring/mvc/security/service/AnnotatedRoleSecuredService.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.spring.mvc.security.service;
 
@@ -10,6 +33,10 @@ import org.springframework.stereotype.Service;
 
 import com.bernardomg.example.spring.mvc.security.auth.PersistentUserDetailsService;
 
+/**
+ * Annotation-based service applying security for diverse roles.
+ *
+ */
 @Service("annotatedRoleSecuredService")
 public class AnnotatedRoleSecuredService implements RoleSecuredService {
 
@@ -19,6 +46,9 @@ public class AnnotatedRoleSecuredService implements RoleSecuredService {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(PersistentUserDetailsService.class);
 
+    /**
+     * Default constructor.
+     */
     public AnnotatedRoleSecuredService() {
         super();
     }
