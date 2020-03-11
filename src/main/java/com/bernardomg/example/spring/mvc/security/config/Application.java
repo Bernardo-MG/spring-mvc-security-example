0ee3dbc372f40f88f5599cf9b2551c84ce195cba
Bernardo <bernardo.martinez@seresco.es>
2020-03-11 15:07:18 +0100

    Added Spring Boot

---
+++ src/main/java/com/bernardomg/example/spring/mvc/security/config/Application.java
@@ -0,0 +1,14 @@
+package com.bernardomg.example.spring.mvc.security.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+
+	public Application()
+	{
+		super();
+	}
+
+}
