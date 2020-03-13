5286b04918c7b3e77aa4927ac77abe3223f737f2
Bernardo <programming@wandrell.com>
2020-03-13 21:55:48 +0100

    Moved configuration to class

---
+++ src/main/java/com/bernardomg/example/spring/mvc/security/config/AuthenticationConfig.java
@@ -0,0 +1,61 @@
+
+package com.bernardomg.example.spring.mvc.security.config;
+
+import javax.servlet.Filter;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+
+@Configuration
+public class AuthenticationConfig {
+
+    public AuthenticationConfig() {
+        super();
+    }
+
+    @Bean("passwordEncoder")
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean("rememberMeFilter")
+    public Filter getRememberMeFilter(
+            final AuthenticationManager authenticationManager,
+            final RememberMeServices rememberMeServices) {
+        return new RememberMeAuthenticationFilter(authenticationManager,
+                rememberMeServices);
+    }
+
+    @Bean("rememberMeServices")
+    public RememberMeServices getRememberMeServices(
+            @Value("${rememberme.key}") final String key,
+            final UserDetailsService userDetailsService,
+            final PersistentTokenRepository tokenRepository) {
+        return new PersistentTokenBasedRememberMeServices(key,
+                userDetailsService, tokenRepository);
+    }
+
+    @Bean("jdbcTokenRepository")
+    public PersistentTokenRepository
+            getJdbcTokenRepository(final DataSource dataSource) {
+        final JdbcTokenRepositoryImpl repo;
+
+        repo = new JdbcTokenRepositoryImpl();
+
+        repo.setDataSource(dataSource);
+
+        return repo;
+    }
+
+}
--- src/main/java/com/bernardomg/example/spring/mvc/security/config/SecurityConfig.java
+++ src/main/java/com/bernardomg/example/spring/mvc/security/config/SecurityConfig.java
@@ -1,17 +1,29 @@
 
 package com.bernardomg.example.spring.mvc.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     public SecurityConfig() {
         super();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
