14fd1af5dcf4315ccb8bac274b2b9aba60379184
Bernardo <programming@wandrell.com>
2020-03-15 13:10:41 +0100

    Added persistence config class

---
+++ src/main/java/com/bernardomg/example/spring/mvc/security/config/PersistenceConfig.java
@@ -0,0 +1,15 @@
+
+package com.bernardomg.example.spring.mvc.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceConfig {
+
+    public PersistenceConfig() {
+        super();
+    }
+
+}
