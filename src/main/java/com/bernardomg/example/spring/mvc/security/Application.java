4e67e4abb27f5e931887352d00d25aba0731e529
Bernardo <programming@wandrell.com>
2020-03-18 15:14:44 +0100

    Removed printing beans

--- src/main/java/com/bernardomg/example/spring/mvc/security/Application.java
+++ src/main/java/com/bernardomg/example/spring/mvc/security/Application.java
@@ -1,26 +1,24 @@
 
 package com.bernardomg.example.spring.mvc.security;
 
-import java.util.Arrays;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(final String[] args) {
-        final ApplicationContext ctx = SpringApplication.run(Application.class,
-                args);
+        // final ApplicationContext ctx =
+        // SpringApplication.run(Application.class,
+        // args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        // System.out.println("Let's inspect the beans provided by Spring
+        // Boot:");
 
-        final String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (final String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        // final String[] beanNames = ctx.getBeanDefinitionNames();
+        // Arrays.sort(beanNames);
+        // for (final String beanName : beanNames) {
+        // System.out.println(beanName);
+        // }
     }
 
     public Application() {
