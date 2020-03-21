545102145dddd2582b08c374814dfe565df98e20
Bernardo <programming@wandrell.com>
2020-03-21 13:42:01 +0100

    Added cache config

---
+++ src/main/java/com/bernardomg/example/spring/mvc/security/config/CacheConfig.java
@@ -0,0 +1,15 @@
+
+package com.bernardomg.example.spring.mvc.security.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    public CacheConfig() {
+        super();
+    }
+
+}
