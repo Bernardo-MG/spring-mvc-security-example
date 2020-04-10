1187d48844f26a3f76e5337a1a8b562fa5d49e58
Bernardo <programming@wandrell.com>
2020-04-10 13:32:26 +0200

    Renamed service and set default username

--- src/main/java/com/bernardomg/example/spring/mvc/security/auth/service/RegisterOAuth2UserService.java
+++ src/main/java/com/bernardomg/example/spring/mvc/security/auth/service/RegisterOAuth2UserService.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017-2018 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.spring.mvc.security.auth.service;
 
@@ -32,22 +55,46 @@ import com.bernardomg.example.spring.mvc.security.user.model.persistence.Persist
 import com.bernardomg.example.spring.mvc.security.user.repository.PersistentRoleRepository;
 import com.bernardomg.example.spring.mvc.security.user.repository.PersistentUserRepository;
 
-public final class CustomOAuth2UserService
+/**
+ * OAUTH 2 user service linked to the DB users. It registers any new user based
+ * on his email.
+ * 
+ * @author Bernardo Martínez Garrido
+ *
+ */
+public final class RegisterOAuth2UserService
         implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     /**
      * Logger.
      */
     private static final Logger            LOGGER   = LoggerFactory
-            .getLogger(CustomOAuth2UserService.class);
+            .getLogger(RegisterOAuth2UserService.class);
 
+    /**
+     * Base service. Applies inheritance through composition.
+     */
     private final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 
+    /**
+     * Roles repository.
+     */
     private final PersistentRoleRepository roleRepository;
 
+    /**
+     * Users repository.
+     */
     private final PersistentUserRepository userRepository;
 
-    public CustomOAuth2UserService(final PersistentUserRepository userRepo,
+    /**
+     * Constructs a service.
+     * 
+     * @param userRepo
+     *            users repository
+     * @param roleRepo
+     *            roles repository
+     */
+    public RegisterOAuth2UserService(final PersistentUserRepository userRepo,
             final PersistentRoleRepository roleRepo) {
         super();
 
@@ -85,6 +132,16 @@ public final class CustomOAuth2UserService
         return new DefaultOAuth2User(mappedAuthorities, attributes, "id");
     }
 
+    /**
+     * Loads the user and returns his authorities.
+     * <p>
+     * If the user exists on DB, the the authorities are taken from it.
+     * Otherwise, the user is created with default authorities.
+     * 
+     * @param oauthuser
+     *            user to load
+     * @return the user authorities
+     */
     private final Collection<GrantedAuthority>
             loadUser(final OAuth2User oauthuser) {
         final Collection<GrantedAuthority> mappedAuthorities;
@@ -94,8 +151,10 @@ public final class CustomOAuth2UserService
         final Iterable<PersistentRole> roles;
         final Collection<String> privileges;
 
+        // TODO: What if somebody creates a fake account with the email? Is it
+        // possible?
         if (oauthuser.getAttributes().containsKey("email")) {
-            email = oauthuser.getAttributes().get("email").toString();
+            email = String.valueOf(oauthuser.getAttributes().get("email"));
             userOpt = userRepository.findOneByEmail(email);
             if (userOpt.isPresent()) {
                 user = userOpt.get();
@@ -104,6 +163,11 @@ public final class CustomOAuth2UserService
                 if (oauthuser.getAttributes().containsKey("name")) {
                     user.setUsername(
                             oauthuser.getAttributes().get("name").toString());
+                } else {
+                    LOGGER.warn(
+                            "OAUTH user {} is missing name field, applying email as name",
+                            oauthuser.getName());
+                    user.setUsername(email);
                 }
                 user.setEmail(email);
 
