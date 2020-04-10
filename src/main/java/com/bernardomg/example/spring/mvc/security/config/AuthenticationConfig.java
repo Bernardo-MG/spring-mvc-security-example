152bf1e277ff1285c771211d2692c9f53eded1b6
Bernardo <programming@wandrell.com>
2020-04-10 13:36:43 +0200

    Added javadocs

--- src/main/java/com/bernardomg/example/spring/mvc/security/config/AuthenticationConfig.java
+++ src/main/java/com/bernardomg/example/spring/mvc/security/config/AuthenticationConfig.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2017-2020 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.spring.mvc.security.config;
 
@@ -20,11 +43,17 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
 
-import com.bernardomg.example.spring.mvc.security.auth.service.CustomOAuth2UserService;
+import com.bernardomg.example.spring.mvc.security.auth.service.RegisterOAuth2UserService;
 import com.bernardomg.example.spring.mvc.security.auth.service.PersistentUserDetailsService;
 import com.bernardomg.example.spring.mvc.security.user.repository.PersistentRoleRepository;
 import com.bernardomg.example.spring.mvc.security.user.repository.PersistentUserRepository;
 
+/**
+ * Authentication configuration.
+ * 
+ * @author Bernardo Martínez Garrido
+ *
+ */
 @Configuration
 public class AuthenticationConfig {
 
@@ -48,7 +77,7 @@ public class AuthenticationConfig {
     public OAuth2UserService<OAuth2UserRequest, OAuth2User>
             getOAuth2UserService(final PersistentUserRepository userRepo,
                     final PersistentRoleRepository roleRepo) {
-        return new CustomOAuth2UserService(userRepo, roleRepo);
+        return new RegisterOAuth2UserService(userRepo, roleRepo);
     }
 
     @Bean("passwordEncoder")
