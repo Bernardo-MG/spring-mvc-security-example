ee72d41070f7afa167d364a6e61ef5f898462a42
Bernardo-MG <programming@bernardo-mg.com>
2023-02-02 22:58:57 +0100

    Updated test data

--- src/test/resources/db/populate/admin_roles_no_privileges.sql
+++ src/test/resources/db/populate/admin_roles_no_privileges.sql
@@ -27,8 +27,8 @@
 -- This SQL script populates the initial data.
 -- ****************************************
 
-INSERT INTO USERS (id, username, password, enabled, locked, expired, credentials_expired) VALUES
-   (1, 'admin',      '$2a$04$gV.k/KKIqr3oPySzs..bx.8absYRTpNe8AbHmPP90.ErW0ICGOsVW',   true,    false,   false,   false);
+INSERT INTO USERS (id, name, email, username, password, enabled, locked, expired, credentials_expired) VALUES
+   (1, 'admin', 'admin@somewhere.com', 'admin', '$2a$04$gV.k/KKIqr3oPySzs..bx.8absYRTpNe8AbHmPP90.ErW0ICGOsVW', true, false, false, false);
 
 INSERT INTO ROLES (id, name) VALUES
    (1, 'ADMIN');
