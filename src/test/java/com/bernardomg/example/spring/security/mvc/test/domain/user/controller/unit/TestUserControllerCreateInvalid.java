4a7ee55b6470ee6156ebc81c7579d5890f5f6cc2
Bernardo-MG <programming@bernardo-mg.com>
2023-04-19 23:28:04 +0200

    Corrected test description

--- src/test/java/com/bernardomg/example/spring/security/mvc/test/domain/user/controller/unit/TestUserControllerCreateInvalid.java
+++ src/test/java/com/bernardomg/example/spring/security/mvc/test/domain/user/controller/unit/TestUserControllerCreateInvalid.java
@@ -73,7 +73,7 @@ public class TestUserControllerCreateInvalid {
     }
 
     @Test
-    @DisplayName("Invalid parameters are rejected")
+    @DisplayName("Empty passwords are rejected")
     public final void testCreate_EmptyPassword() throws Exception {
         final RequestBuilder request; // Test request
 
